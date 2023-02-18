ce6bf04685366067d198e4b08c68a07611ba7ba1
KlejdyszS <grawerjk@gmail.com>
2023-02-18 21:48:00 +0100

    Working adding multiple world:number values in model field

--- ordersmanager/src/components/OrderForm.js
+++ ordersmanager/src/components/OrderForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OrderPreview from './OrderPreview';
 import { formFields, statusOptions } from '../constants';
 
@@ -9,6 +9,32 @@ const OrderForm = ({
     handleAddOrder,
     handleUpdateOrder,
 }) => {
+    const [modelPairs, setModelPairs] = useState([]);
+
+    const handleAddPair = () => {
+        setModelPairs([...modelPairs, { word: '', number: '' }]);
+    };
+
+    const handlePairInputChange = (index, event) => {
+        const { name, value } = event.target;
+        const newPairs = [...modelPairs];
+        newPairs[index][name] = value;
+        setModelPairs(newPairs);
+        const modelFieldValue = getModelFieldValue();
+        handleInputChange({
+            target: {
+                name: 'model',
+                value: modelFieldValue,
+            },
+        });
+    };
+
+    const getModelFieldValue = () => {
+        return modelPairs
+            .map((pair) => `${pair.word}:${pair.number}`)
+            .join(';');
+    };
+ 
     return (
         <form
             onSubmit={newOrder._id ? () => handleUpdateOrder(newOrder._id) : handleAddOrder}
@@ -18,21 +44,72 @@ const OrderForm = ({
                 <h2 className="text-2xl font-medium mb-4 text-center">
                     {newOrder._id ? 'Edit Order' : 'Add New Order'}
                 </h2>
-                {formFields.map((field) => (
-                    <div key={field.id} className="mb-4">
-                        <label htmlFor={field.name} className="block text-gray-700 font-bold mb-2">
-                            {field.label}:
-                        </label>
-                        <input
-                            type={field.type}
-                            id={field.name}
-                            name={field.name}
-                            value={newOrder[field.name]}
-                            onChange={handleInputChange}
-                            className="border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 rounded-md shadow-sm w-full py-2 px-3"
-                        />
-                    </div>
-                ))}
+                {formFields.map((field) => {
+                    if (field.name === 'model') {
+                        return (
+                            <div key={field.id} className="mb-4">
+                                <label
+                                    htmlFor={field.name}
+                                    className="block text-gray-700 font-bold mb-2"
+                                >
+                                    {field.label}:
+                                </label>
+                                <div>
+                                    {modelPairs.map((pair, index) => (
+                                        <div key={index} className="mb-2">
+                                            <input
+                                                type="text"
+                                                name="word"
+                                                placeholder="Word"
+                                                value={pair.word}
+                                                onChange={(event) =>
+                                                    handlePairInputChange(index, event)
+                                                }
+                                                className="border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 rounded-md shadow-sm w-1/2 py-2 px-3 mr-2"
+                                            />
+                                            <input
+                                                type="text"
+                                                name="number"
+                                                placeholder="Number"
+                                                value={pair.number}
+                                                onChange={(event) =>
+                                                    handlePairInputChange(index, event)
+                                                }
+                                                className="border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 rounded-md shadow-sm w-1/2 py-2 px-3"
+                                            />
+                                        </div>
+                                    ))}
+                                    <button
+                                        type="button"
+                                        onClick={handleAddPair}
+                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    >
+                                        Add
+                                    </button>
+                                </div>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div key={field.id} className="mb-4">
+                                <label
+                                    htmlFor={field.name}
+                                    className="block text-gray-700 font-bold mb-2"
+                                >
+                                    {field.label}:
+                                </label>
+                                <input
+                                    type={field.type}
+                                    id={field.name}
+                                    name={field.name}
+                                    value={newOrder[field.name]}
+                                    onChange={handleInputChange}
+                                    className="border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 rounded-md shadow-sm w-full py-2 px-3"
+                                />
+                            </div>
+                        );
+                    }
+                })}
                 <div className="mb-4">
                     <label htmlFor="status" className="block text-gray-700 font-bold mb-2">
                         Status:
@@ -65,4 +142,4 @@ const OrderForm = ({
     );
 };
 
-export default OrderForm;
+    export default OrderForm;
--- ordersmanager/src/components/OrderPreview.js
+++ ordersmanager/src/components/OrderPreview.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 function OrderPreview({ order }) {
+    const modelPairs = order.model ? order.model.split(';') : [];
+  
     return (
         <div className="flex flex-col bg-gray-100 p-4 rounded-md shadow-md">
             <h2 className="text-2xl font-medium mb-4 text-center">
@@ -8,7 +10,21 @@ function OrderPreview({ order }) {
             </h2>
             <p><span className="font-bold">Email:</span> {order.email}</p>
             <p><span className="font-bold">Quantity:</span> {order.quantity}</p>
-            <p><span className="font-bold">Model:</span> {order.model}</p>
+            {modelPairs.length > 0 && (
+                <div>
+                    <span className="font-bold">Model:</span>
+                    <ul>
+                        {modelPairs.map((pair, index) => {
+                            const [word, number] = pair.split(':');
+                            return (
+                                <li key={index}>
+                                    {word}: {number}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            )}
             <p><span className="font-bold">Color:</span> {order.color}</p>
             <p><span className="font-bold">Status:</span> {order.status}</p>
         </div>
